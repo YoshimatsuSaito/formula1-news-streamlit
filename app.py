85e0b662f471afbe7a90b77350ff93e937b94cf5
YoshimatsuSaito <gashganriughnreipu@gmail.com>
2022-02-27 03:03:06 +0000

    [update]

--- app.py
+++ app.py
@@ -15,15 +15,18 @@ def main():
             )
     with st.spinner('Now scraping...'):
         ns.get_news_info()
-        for site_name in ns.df_today_news["site_name"].unique():
-            df_target = ns.df_today_news[ns.df_today_news["site_name"]==site_name].reset_index(drop=True)
-            st.subheader(site_name)
-            for idx in range(len(df_target)):
-                news_link = df_target.iloc[idx]["news_link"]
-                news_title = df_target.iloc[idx]["news_title"]
-                st.markdown(
-                    f"<a href='{news_link}' target='_blank' rel='noopener noreferrer'>{news_title}</a>", unsafe_allow_html=True,
-                )
+        if len(ns.df_today_news["site_name"]) > 0:
+            for site_name in ns.df_today_news["site_name"].unique():
+                df_target = ns.df_today_news[ns.df_today_news["site_name"]==site_name].reset_index(drop=True)
+                st.subheader(site_name)
+                for idx in range(len(df_target)):
+                    news_link = df_target.iloc[idx]["news_link"]
+                    news_title = df_target.iloc[idx]["news_title"]
+                    st.markdown(
+                        f"<a href='{news_link}' target='_blank' rel='noopener noreferrer'>{news_title}</a>", unsafe_allow_html=True,
+                    )
+        else:
+            st.markdown("Nothing so far")
 
 if __name__ == "__main__":
     main()
